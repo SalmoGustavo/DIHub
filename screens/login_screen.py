9f8304fb429411c6ebd3f3acf6c938686709ec7f
SalmoGustavo <aguilargustavo564@gmail.com>
2024-05-18 18:25:15 -0300

    Deploy

--- screens/login_screen.py
+++ screens/login_screen.py
@@ -16,23 +16,37 @@ class LoginScreen:
 
         ct.CTkLabel(self.window, text=' ').pack()
 
-        widimage = ct.CTkLabel(self.window, image=image, text=' ')
+        widimage = ct.CTkLabel(self.window, image=image, text=' ', pady=50)
         widimage.pack()
 
-        entry_username = ct.CTkEntry(self.window, placeholder_text='Insira seu nome de usuário...',
-                                     width=280)
-        entry_username.pack(padx=10, pady=(self.window.winfo_height() / 2, 10))
+        self.status = ct.CTkLabel(self.window, text='', font=('default', 20), text_color='red')
+        self.status.pack()
 
-        entry_password = ct.CTkEntry(self.window, placeholder_text='Insira sua senha...',
-                                     width=280)
-        entry_password.pack(padx=10, pady=10)
+        self.entry_username = ct.CTkEntry(self.window, placeholder_text='Insira seu nome de usuário...', width=280)
+        self.entry_username.pack(padx=10, pady=10)
 
-        login_button = ct.CTkButton(self.window, text='Entrar')
+        self.entry_password = ct.CTkEntry(self.window, placeholder_text='Insira sua senha...',
+                                     width=280)
+        self.entry_password.pack(padx=10, pady=10)
+
+        login_button = ct.CTkButton(self.window, text='Entrar', command=self.login)
         login_button.pack()
 
     def run(self):
         self.window.mainloop()
 
+    def login(self):
+        username = self.entry_username.get()
+        password = self.entry_password.get()
+
+        print(f'Username: {username}\nPassword: {password}')
+
+        if username != 'salmogustavo' or password != '2808':
+            self.status.configure(text_color='red')
+            self.status.configure(text='Usuário ou senha incorretos.')
+        else:
+            self.status.configure(text_color='lightgreen')
+            self.status.configure(text='Bem vindo!')
 
 # Criar instância e executar a aplicação
 app = LoginScreen()
